83cf4fa107582df1c2ade1040a2b2bbafe3547a2
Jzh09 <Tejada.joshua09@gmailcom>
2024-05-03 23:00:34 +0800

    update backend

--- server/index.js
+++ server/index.js
@@ -11,10 +11,41 @@ const db = mysql.createPool({
     database:"LUTO"
 })
 
-app.get("/", (req, res) => {
-    res.json("good mou.")
-})
+    app.post('/signup', (req, res) => {
+        const { username, email, password } = req.body;
+        const registration_date = new Date();
+        const query = 'INSERT INTO users (username, password, registration_date,email) VALUES (?, ?, ?, ?)'
 
+        db.query(query, [username, password, registration_date,email], (err, result) => {
+            if (err) {
+              res.status(500).send('Error creating user');
+            } else {
+              res.status(200).send('User created successfully');
+            }
+
+          });
+        });    
+
+        app.post('/signin', (req, res) => {
+            const { username, password } = req.body;
+            const query = 'SELECT * FROM users WHERE username = ? AND password = ?'
+          
+            db.query(query, [username, password], (err, result) => {
+                if (err) {
+                  res.status(500).send('Error retrieving user');
+                } else {
+                  if (result.length > 0) {
+                    res.status(200).send('Sign in successful');
+                  } else {
+                    res.status(401).send('Incorrect username or password');
+                  }
+                }
+              });
+            });     
 app.listen(PORT, () => {
     console.log("Connected to backend!");
+
+
+
 })
+
